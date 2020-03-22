75f21ae002ebf183f7ad30a1465d0d90f2b6f377
indogusmas <indogusmasarungsamudra@gmail.com>
2020-03-22 11:38:32 +0700

    initial commit

---
+++ lib/DetailPage.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+
+class DetailPage extends StatefulWidget{
+  @override
+  _DetailPageState createState() => _DetailPageState();
+
+}
+
+class _DetailPageState extends State<DetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[],
+      ),
+    );
+  }
+}
